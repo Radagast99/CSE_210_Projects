853480029adeef60ed70305d7467579484725288
Efraim Lopez BYU <u440313879@byui.edu>
2023-01-04 23:10:46 -0600

    Learning Activity - C# Prep 2, Assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Please enter your grade percentage here: ");
+        string grade = Console.ReadLine();
+
+        int gradePer = int.Parse(grade);
+
+        string letter = "";
+
+        if (gradePer >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePer >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePer >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePer >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.Write($"Your score is: {letter}");
+
+        if (gradePer >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else 
+        {
+             Console.WriteLine("I hope you can have a better grade in the future");
+        }
+
+    }   
+
 }
