f17cea7d4acfa2345ffd3811a6070ac1fbd583df
Efraim Lopez BYU <u440313879@byui.edu>
2023-01-05 22:54:32 -0600

    Learning Activity - C# Prep 4 assigment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,43 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {   //declaring the game loop variable 
+        string gameContinue;
+        do
+        {
+            // Creating the do while loop
+            // declaring randomizer
+            Random randomGenerator = new Random();
+            // Declaring the variables for the random magic number
+            int magicNumber = randomGenerator.Next(1, 101);
+            // Declaring the variable for the guessNumber
+            int guessNumber = 102;
+            // doing the while loop
+            while (guessNumber != magicNumber) 
+            {   
+                // asking user's guess
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                // creating conditions 
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > magicNumber) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Asking user if wants to continue the game.
+            Console.WriteLine("");
+            Console.Write("Would you like to play again? ");
+            gameContinue = Console.ReadLine();
+        } while (gameContinue == "yes");
+    } 
 }
+
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   // declaring variable for do-while
+        int listFinisher;
+        // creating list and program instructions.
+        List<int> numberList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        // declaring the do-while loop.
+        do 
+        {   
+            // asking a number to the user.
+            Console.Write("Enter a number: ");
+            listFinisher = int.Parse(Console.ReadLine());
+            //exclude the zero from the list
+            if (listFinisher != 0)
+            {
+                numberList.Add(listFinisher);
+            }
+
+        } while (listFinisher != 0);
+        // computing the list sum.
+        int total = numberList.Sum();
+        Console.WriteLine($"The sum is: {total}"); 
+        // computing the average number from the list.
+        int listLen = numberList.Count;
+        float average = total / listLen;
+        Console.WriteLine($"The average is: {average}");
+        // computing the largest number from te list.
+        int largestNum = numberList.Max();
+        Console.WriteLine($"The largest number is: {largestNum}");
     }
 }
